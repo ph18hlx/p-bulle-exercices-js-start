a05faa843a8b3b95070f4b6b977218b414d5e9b3
Thibaud Blasco Dias <ph18hlx@eduvaud.ch>
2026-01-22 16:22:51 +0100

    chore(exercice):freelance

--- exercices/freelancer-rates/freelancer-rates.js
+++ exercices/freelancer-rates/freelancer-rates.js
@@ -26,7 +26,7 @@
  * @returns {number} the rate per day
  */
 export function dayRate(ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  return ratePerHour * 8;
 }
 
 /**
@@ -37,7 +37,7 @@ export function dayRate(ratePerHour) {
  * @returns {number} the number of days
  */
 export function daysInBudget(budget, ratePerHour) {
-  throw new Error('Remove this line and implement the function');
+  return Math.floor(budget / dayRate(ratePerHour));
 }
 
 /**
@@ -49,5 +49,16 @@ export function daysInBudget(budget, ratePerHour) {
  * @returns {number} the rounded up discounted rate
  */
 export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
-  throw new Error('Remove this line and implement the function');
+   const daysPerMonth = 22;
+  const dailyRate = dayRate(ratePerHour);
+
+  const fullMonths = Math.floor(numDays / daysPerMonth);
+  const remainingDays = numDays % daysPerMonth;
+
+  const discountedMonthlyCost =
+    fullMonths * daysPerMonth * dailyRate * (1 - discount);
+
+  const remainingCost = remainingDays * dailyRate;
+
+  return Math.ceil(discountedMonthlyCost + remainingCost);
 }
